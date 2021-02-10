7e0340941378ffc93b1244a6ed805557e85c09d7
Fedir RYKHTIK <feodor.rykhtik@gmail.com>
2021-02-10 22:51:29 +0100

    Reverse String with StringBuilder

--- reverse-string/src/main/java/ReverseString.java
+++ reverse-string/src/main/java/ReverseString.java
@@ -1,12 +1,7 @@
-
 class ReverseString {
 
     String reverse(String inputString) {
-        String output= "";
-        for (int i = 0; i < inputString.length(); i++) {
-            output += inputString.charAt(inputString.length()-i-1);
-        }
-        return output;
+        return new StringBuilder(inputString).reverse().toString();
     }
   
 }
